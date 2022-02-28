760ab326ac73147e5faffe1f5065f287d0e45dce
Daniel Molina <damolina@outlook.com>
2022-02-28 12:34:56 -0800

    Ad basic test for template registration.

---
+++ src/test/suite/templates.test.ts
@@ -0,0 +1,39 @@
+import * as assert from 'assert';
+import { basename } from 'path';
+import { Template } from '../../schemas';
+import { FileEditContext, TemplateEditContext } from '../../templateEdit';
+import { getRegisteredTemplates, registerTemplate, unregisterTemplate } from '../../templates';
+
+import { replaceFileTemplateLevelVariablesAsync, replaceTemplateLevelVariablesAsync } from '../../variableReplacer';
+
+suite(`Suite: ${basename(__filename)}: replaceFileTemplateLevelVariablesAsync`, () => {
+  let restorables: Array<{ restore: () => void }>;
+
+  setup(() => {
+    restorables = [];
+  });
+
+  teardown(() => {
+    restorables.forEach((r) => r.restore());
+  });
+
+  test('getRegisteredTemplates / registerTemplate / unregisterTemplate', () => {
+    const template: Template = {
+      location: "test location",
+      name: "test name",
+      files: [],
+    };
+    const templateAsJSON = JSON.stringify(template);
+    const templateId = "test:templateId";
+
+    assert.strictEqual(getRegisteredTemplates().size, 0);
+    registerTemplate(templateId, templateAsJSON);
+
+    const registeredTemplates = getRegisteredTemplates();
+    assert.strictEqual(registeredTemplates.size, 1);
+    assert.deepStrictEqual(registeredTemplates.get(templateId), template);
+
+    unregisterTemplate(templateId);
+    assert.strictEqual(getRegisteredTemplates().size, 0);
+  });
+});
